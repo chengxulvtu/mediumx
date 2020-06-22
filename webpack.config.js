3f5a440293ab34e58c294d9e3c64bc4da69b0432
Chan Gavin <justin.coding@gmail.com>
2020-06-22 11:02:20 +0800

    重构项目

---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+const path = require("path");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
+
+module.exports = {
+  entry: {
+    background: path.resolve(__dirname, "src/background.js")
+    // main: path.resolve(__dirname, "src/medium-enhanced-reader.js")
+  },
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "[name].bundle.js"
+  },
+  mode: "development",
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      }
+    ]
+  },
+  plugins: [
+    new CopyWebpackPlugin({
+      patterns: [
+        { from: "static", to: "." },
+        { from: "manifest.json", to: "manifest.json" }
+      ]
+    })
+  ]
+};
